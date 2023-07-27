2bcaafb837722f374cc9d834263b5d2b5ad776e0
Alexis Mauricio Gunsett <mau_gunsett@hotmail.com>
2023-07-27 11:55:12 -0300

    Termino el punto 11

--- js/punto1.js
+++ js/punto1.js
@@ -20,13 +20,3 @@ if(isNaN(numero)) {
     console.log("No puede conducir")
 }
 
-let drinkCoffee = 0
-
-while (drinkCoffee < 10) {
-  drinkCoffee = drinkCoffee + 1
-  
-  if (drinkCoffee === 8) { break }
-  if (drinkCoffee === 5) { continue }
-
-  console.log('Café')
-}
--- js/punto11.js
+++ js/punto11.js
@@ -11,3 +11,38 @@ Nota: ver funcion Math() https://www.w3schools.com/js/js_math.asp
     INDICAR LA MAYOR
 */
 
+let nombre1;
+nombre1 = prompt("Ingrese su nombre");
+let edad1;
+edad1 = prompt("Ingrese su edad");
+
+let nombre2;
+nombre2 = prompt("Ingrese su nombre");
+let edad2;
+edad2 = prompt("Ingrese su edad");
+
+let nombre3;
+nombre3 = prompt("Ingrese su nombre");
+let edad3;
+edad3 = prompt("Ingrese su edad");
+
+let num1 = Number(edad1);
+let num2 = Number(edad2);
+let num3 = Number(edad3);
+
+if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
+  alert("En edad debes ingresar un número");
+} else {
+  let mayor = Math.max(num1, num2, num3);
+
+  if (mayor === num1) {
+    console.log(`El mayor es: ${nombre1}`);
+  } else if (mayor === num2) {
+    console.log(`El mayor es: ${nombre2}`);
+  } else {
+    console.log(`El mayor es: ${nombre3}`);
+  }
+}
+
+
+
